f04ae5827ce1ccb4ecefc2e1d5d1b9a2d02320cf
Alistair Lynn <arplynn@gmail.com>
2022-08-09 20:26:18 +0000

    Write the 2022 scorer

--- scoring/score.py
+++ scoring/score.py
@@ -1,9 +1,8 @@
-POINTS_FOR_LOCATION = {
-    # Location -> get points callable
-    'arena': lambda _: 0,
-    'docking_area': lambda _: 1,
-    'zone_0_raised_area': lambda token_owner: 3 if token_owner == 0 else 0,
-    'zone_1_raised_area': lambda token_owner: 3 if token_owner == 1 else 0,
+STARTING_ZONES_BY_LETTER = {
+    'G': 0,
+    'O': 1,
+    'P': 2,
+    'Y': 3,
 }
 
 
@@ -13,35 +12,92 @@ class Scorer:
     # equivalent-time operations to the token controller.
 
     def __init__(self, teams_data, arena_data):
-        self._zone_to_tla = {
+        self._starting_zone_to_tla = {
             info['zone']: tla
             for tla, info in teams_data.items()
         }
 
-        # Token Claims look like this:
-        # {
-        #     'zone': 0 | 1,
-        #     'token_index': int,
-        #     'location': 'arena' | 'docking_area' | 'zone_0_raised_area' | 'zone_1_raised_area',
-        #     'time': float,
-        # }
-        self._token_claims = arena_data['other']['token_claims']
+        self._team_info = teams_data
+
+        self._scoring_zones = arena_data['arena_zones']
 
     def calculate_scores(self):
-        # Mapping from token -> (owning zone, location)
-        end_state = {
-            claim['token_index']: (claim['zone'], claim['location'])
-            for claim in self._token_claims
+        points_per_team = {
+            tla: 0
+            for tla in self._team_info
         }
 
-        points_per_zone = {0: 0, 1: 0}
-        for owner, location in end_state.values():
-            points_per_zone[owner] += POINTS_FOR_LOCATION[location](owner)
+        # Point for movement
+        for tla, info in self._team_info.items():
+            if info.get('moved'):
+                points_per_team[tla] += 1
 
-        return {
-            tla: points_per_zone[zone]
-            for zone, tla in self._zone_to_tla.items()
+        # Scoring zone weighting
+        weight_by_scoring_zone_and_starting_zone = {
+            (scoring_zone, starting_zone): 0
+            for scoring_zone in range(9)
+            for starting_zone in range(4)
         }
+
+        # Add up the weights
+        for scoring_zone, scoring_zone_configuration in self._scoring_zones.items():
+            for weighting, letter in (
+                [(1, letter) for letter in scoring_zone_configuration['partial']] +
+                [(2, letter) for letter in scoring_zone_configuration['full']]
+            ):
+                starting_zone = STARTING_ZONES_BY_LETTER[letter]
+                weight_by_scoring_zone_and_starting_zone[scoring_zone, starting_zone] += weighting
+
+        # Compute the maximum weight per scoring zone
+        maximum_weight_by_scoring_zone = {
+            scoring_zone: max(
+                weight_by_scoring_zone_and_starting_zone[scoring_zone, starting_zone]
+                for starting_zone in range(4)
+            )
+            for scoring_zone in range(9)
+        }
+
+        # Peak starting zones by scoring zone
+        peak_starting_zones_by_scoring_zone = {n: [] for n in range(9)}
+
+        for scoring_zone in range(9):
+            for starting_zone in range(4):
+                if (
+                    weight_by_scoring_zone_and_starting_zone[scoring_zone, starting_zone] ==
+                    maximum_weight_by_scoring_zone[scoring_zone]
+                ) and (
+                    maximum_weight_by_scoring_zone[scoring_zone] > 0
+                ):
+                    peak_starting_zones_by_scoring_zone[scoring_zone].append(starting_zone)
+    
+        # Peak teams by scoring zone
+        peak_teams_by_scoring_zone = {
+            scoring_zone: [
+                self._starting_zone_to_tla[x] for x in peak_starting_zones
+            ]
+            for scoring_zone, peak_starting_zones in peak_starting_zones_by_scoring_zone.items()
+        }
+
+        # Number of owned zones by team
+        owned_zone_count_by_team = {
+            tla: 0
+            for tla in self._team_info
+        }
+
+        for scoring_zone, peak_teams in peak_teams_by_scoring_zone.items():
+            for peak_team in peak_teams:
+                owned_zone_count_by_team[peak_team] += 1
+
+        # Owned zone score
+        owned_zone_score_by_team = {
+            team: zone_count ** 2
+            for team, zone_count in owned_zone_count_by_team.items()
+        }
+    
+        for team in self._team_info:
+            points_per_team[team] += owned_zone_score_by_team[team]
+
+        return points_per_team
 
 
 if __name__ == '__main__':
